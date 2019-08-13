a460882d89d126853e449c253f9cb810670593fd
hugofvferreiar97- <hugofvferreira97@gmail.com>
2019-08-13 18:43:57 +0100

    First Commit

--- src/test/java/ESIIFunctionsTest.java
+++ src/test/java/ESIIFunctionsTest.java
@@ -11,7 +11,7 @@ public class ESIIFunctionsTest {
     }
 
     @Test
-    void failReturnOne() {
+    public void failReturnOne() {
         assertEquals(2, new ESIIFunctions().returnOne());
     }
 }
